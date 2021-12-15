521cf75f51259c78a3a7839acc128b954cddbe79
Daniel Tahko <daniel.tahko@hotmail.com>
2021-12-15 11:51:45 +0100

    merge sort

--- grupp.cpp
+++ grupp.cpp
@@ -145,7 +145,7 @@ void heapSort(vector<int>array, int n)
 
 int partition(vector<int>array, int low, int high) {
     int pivot = array[high];
-    int i = (low - 1);
+    int i = (low-1);
     for (int j = low; j <= high-1; j++) {
         if (array[j] <= pivot) {
             i++;
@@ -163,6 +163,67 @@ void quickSort(vector<int>array, int low, int high) {
         quickSort(array, low, pi -1);
         quickSort(array, pi+1, high);
     }
+}
+
+void merge(vector<int>array, int const left, int const mid, int const right)
+{
+	auto const subArrayOne = mid - left + 1;
+	auto const subArrayTwo = right - mid;
+
+	// Create temp arrays
+	auto *leftArray = new int[subArrayOne],
+		*rightArray = new int[subArrayTwo];
+
+	// Copy data to temp arrays leftArray[] and rightArray[]
+	for (auto i = 0; i < subArrayOne; i++)
+		leftArray[i] = array[left + i];
+	for (auto j = 0; j < subArrayTwo; j++)
+		rightArray[j] = array[mid + 1 + j];
+
+	auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+		indexOfSubArrayTwo = 0; // Initial index of second sub-array
+	int indexOfMergedArray = left; // Initial index of merged array
+
+	// Merge the temp arrays back into array[left..right]
+	while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+		if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+			array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+			indexOfSubArrayOne++;
+		}
+		else {
+			array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+			indexOfSubArrayTwo++;
+		}
+		indexOfMergedArray++;
+	}
+	// Copy the remaining elements of
+	// left[], if there are any
+	while (indexOfSubArrayOne < subArrayOne) {
+		array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+		indexOfSubArrayOne++;
+		indexOfMergedArray++;
+	}
+	// Copy the remaining elements of
+	// right[], if there are any
+	while (indexOfSubArrayTwo < subArrayTwo) {
+		array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+		indexOfSubArrayTwo++;
+		indexOfMergedArray++;
+	}
+}
+
+// begin is for left index and end is
+// right index of the sub-array
+// of arr to be sorted */
+void mergeSort(vector<int>array, int const begin, int const end)
+{
+	if (begin >= end)
+		return; // Returns recursively
+
+	auto mid = begin + (end - begin) / 2;
+	mergeSort(array, begin, mid);
+	mergeSort(array, mid + 1, end);
+	merge(array, begin, mid, end);
 }
 
 vector<int> createArray(int input) {
@@ -226,6 +287,11 @@ void checkAll(Time t, vector<int> array, int size) {
     t.startCount();
     quickSort(array, low, high);
     cout << "Quicksort =" << endl;
+    t.getTime();
+
+    t.startCount();
+    mergeSort(array, 0, size - 1);
+    cout << "Merge sort =" << endl;
     t.getTime();
 }
 
@@ -301,3 +367,10 @@ for (int i = 0; i < size; i++) {
     }
     cout << endl;
 */
+
+
+
+
+
+
+

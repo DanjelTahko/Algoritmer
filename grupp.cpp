94f04b13034b4a8689e9aad61a454e5cc5ebcd06
Daniel Tahko <daniel.tahko@hotmail.com>
2021-12-15 10:57:50 +0100

    <=

--- grupp.cpp
+++ grupp.cpp
@@ -147,7 +147,7 @@ int partition(vector<int>array, int low, int high) {
     int pivot = array[high];
     int i = (low - 1);
     for (int j = low; j <= high-1; j++) {
-        if (array[j] < pivot) {
+        if (array[j] <= pivot) {
             i++;
             swap(&array[i], &array[j]);
         }
